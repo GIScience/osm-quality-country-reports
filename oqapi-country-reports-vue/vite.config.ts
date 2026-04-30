e56551ddf98669e2c0fdd44e568734fdd352a42a
vboehmer <valentin.boehmer@heigit.org>
2026-04-30 11:52:57 +0200

    feat: set up vue project

--- oqapi-country-reports-vue/vite.config.ts
+++ oqapi-country-reports-vue/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // Use env var for local override, otherwise default to GH Pages path
-const base = process.env.VITE_BASE_URL || '/humanitarian_pages/oqapi-country-report/'
+const base = process.env.VITE_BASE_URL || '/osm-quality-country-reports/'
 
 export default defineConfig({
   base,
