531d4cbae04fec8e19ee8c6bc5b3a7207fbc55f1
vboehmer <valentin.boehmer@heigit.org>
2026-06-16 11:33:00 +0200

    renamed projectfolders

--- src/osm_quality_pipeline/defs/assets/geoboundary.py
+++ src/osm_quality_pipeline/defs/assets/geoboundary.py
@@ -1,7 +1,7 @@
 import os
 import dagster as dg
-from refactored_pipeline.defs.assets.utils import download_from_geoboundaries
-from refactored_pipeline.defs.partitions import country_partitions
+from src.osm_quality_pipeline.defs.assets.utils import download_from_geoboundaries
+from src.osm_quality_pipeline.defs.partitions import country_partitions
 
 
 @dg.asset(
