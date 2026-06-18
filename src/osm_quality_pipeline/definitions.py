043e1507d9911571e8ad9a5e7f43c8f56db8b5f4
Marcel Reinmuth <marcel.reinmuth@heigit.org>
2026-06-18 14:18:13 +0200

    ref: return registered resources at the end of resources instead in definitions

--- src/osm_quality_pipeline/definitions.py
+++ src/osm_quality_pipeline/definitions.py
@@ -1,14 +1,8 @@
 from pathlib import Path
 
-import dagster as dg
 from dagster import definitions, load_from_defs_folder
-
-from osm_quality_pipeline.defs.resources import OhsomeQualityApiResource
 
 
 @definitions
 def defs():
-    return dg.Definitions.merge(
-        load_from_defs_folder(project_root=Path(__file__).parent.parent.parent),
-        dg.Definitions(resources={"ohsome_api": OhsomeQualityApiResource()}),
-    )
+    return load_from_defs_folder(project_root=Path(__file__).parent.parent.parent)
