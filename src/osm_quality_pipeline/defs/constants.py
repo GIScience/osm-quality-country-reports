809c53b61da7b52561b709084dafbff3bd677d24
Sylvia <sylvia.pscheidl@heigit.org>
2026-06-23 16:51:22 +0200

    for pathlib add repo_root path and data_dir path

--- src/osm_quality_pipeline/defs/constants.py
+++ src/osm_quality_pipeline/defs/constants.py
@@ -1,4 +1,5 @@
 import dagster as dg
+from pathlib import Path
 
 H3_ZOOM_LEVEL = None
 
@@ -16,3 +17,6 @@ ADM_LEVELS = ["ADM0", "ADM1"]
 class ApiRequestConfig(dg.Config):
     max_workers: int = 5
     handle_500_as_na: bool = False
+
+REPO_ROOT = Path(__file__).parent.parent.parent.parent
+DATA_DIR = REPO_ROOT / "data"
