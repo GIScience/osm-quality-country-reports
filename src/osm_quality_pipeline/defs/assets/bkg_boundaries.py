daeafd8200c2f497f3a8ff2c08b5af722d418dd0
Sylvia <sylvia.pscheidl@heigit.org>
2026-06-23 17:53:28 +0200

    add pathlib to geoboundary_bkg asset

--- src/osm_quality_pipeline/defs/assets/bkg_boundaries.py
+++ src/osm_quality_pipeline/defs/assets/bkg_boundaries.py
@@ -1,29 +1,29 @@
-import os
 import io
 import dagster as dg
 import requests
 import sys
 import zipfile
 import geopandas as gpd
+from src.osm_quality_pipeline.defs.constants import DATA_DIR
 
 
 @dg.asset
 def geoboundary_bkg() -> dg.Output[str]:
-    out_dir = os.path.join("data/germany")
-    os.makedirs(out_dir, exist_ok=True)
-
     list_url = "https://daten.gdz.bkg.bund.de/produkte/vg/vg25_ebenen/aktuell/vg25.utm32s.gpkg.zip"
     try:
-        download_bkg_boundaries(list_url=list_url, level_val="vg25_sta", out_dir=out_dir)
+        bkg_boundaries = download_bkg_boundaries(list_url=list_url, level_val="vg25_sta")
     except SystemExit as e:
         print(f"BKG boundaries download failed: {e}")
         raise Exception(f"Failed to fetch boundaries for Germany from BKG.")
-    return dg.Output(out_dir)
-
-geoboundary_bkg()
+    return dg.Output(str(bkg_boundaries))
 
 
-def download_bkg_boundaries(list_url, level_val, out_dir):   #layer: vg25_sta, vg25_lan, vg25_gem
+def download_bkg_boundaries(list_url, level_val):   #layer: vg25_sta, vg25_lan, vg25_gem
+    germany_dir = DATA_DIR / "germany"
+    germany_dir.mkdir(parents=True, exist_ok=True)
+    gpkg_path = germany_dir / "DE_VG25.gpkg"
+    out_path = germany_dir / f"{level_val}.geojson"
+
     try:
         resp = requests.get(list_url)
         resp.raise_for_status()
@@ -33,15 +33,8 @@ def download_bkg_boundaries(list_url, level_val, out_dir):   #layer: vg25_sta, v
 
     with zipfile.ZipFile(io.BytesIO(resp.content)) as z:
         zip_path = "daten/DE_VG25.gpkg"
-
-    gpkg_path = os.path.join(out_dir, "DE_VG25.gpkg")
-
-    with z.open(zip_path) as source:
-        with open(gpkg_path, "wb") as target:
-            target.write(source.read())
-
-
-    out_path = os.path.join(out_dir, f"{level_val}.geojson")
+        with z.open(zip_path) as source:
+            gpkg_path.write_bytes(source.read())
 
     gdf = (
         gpd.read_file(gpkg_path, layer=level_val)
@@ -50,3 +43,7 @@ def download_bkg_boundaries(list_url, level_val, out_dir):   #layer: vg25_sta, v
 
     gdf = gdf[gdf.geometry.notnull() & gdf.is_valid]
     gdf.to_file(out_path, driver="GeoJSON")
+
+    gpkg_path.unlink()
+
+    return(out_path)
