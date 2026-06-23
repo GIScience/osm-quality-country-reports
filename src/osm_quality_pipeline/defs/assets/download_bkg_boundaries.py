ca977ae2714b60e08df20ab017ba362c38b8f024
Sylvia <sylvia.pscheidl@heigit.org>
2026-06-23 11:01:08 +0200

    add asset for downloading bkg boundaries

--- src/osm_quality_pipeline/defs/assets/download_bkg_boundaries.py
+++ src/osm_quality_pipeline/defs/assets/download_bkg_boundaries.py
@@ -1,8 +1,8 @@
 import os
 import io
 import dagster as dg
-from utils import download_bkg_boundaries
 import requests
+import sys
 import zipfile
 import geopandas as gpd
 
@@ -21,3 +21,32 @@ def geoboundary_bkg() -> dg.Output[str]:
     return dg.Output(out_dir)
 
 geoboundary_bkg()
+
+
+def download_bkg_boundaries(list_url, level_val, out_dir):   #layer: vg25_sta, vg25_lan, vg25_gem
+    try:
+        resp = requests.get(list_url)
+        resp.raise_for_status()
+    except requests.RequestException as e:
+        print(f"ERROR fetching boundary list: {e}", file=sys.stderr)
+        sys.exit(1)
+
+    with zipfile.ZipFile(io.BytesIO(resp.content)) as z:
+        zip_path = "daten/DE_VG25.gpkg"
+
+    gpkg_path = os.path.join(out_dir, "DE_VG25.gpkg")
+
+    with z.open(zip_path) as source:
+        with open(gpkg_path, "wb") as target:
+            target.write(source.read())
+
+
+    out_path = os.path.join(out_dir, f"{level_val}.geojson")
+
+    gdf = (
+        gpd.read_file(gpkg_path, layer=level_val)
+        .to_crs(4326)
+    )
+
+    gdf = gdf[gdf.geometry.notnull() & gdf.is_valid]
+    gdf.to_file(out_path, driver="GeoJSON")
