52e4f70ac8741a4a19833c83513d0695690ba7f9
Benjamin Herfort <Hagellach37@users.noreply.github.com>
2026-06-26 15:29:49 +0200

    refactor country layer asset

--- src/osm_quality_pipeline/defs/assets/country_layers.py
+++ src/osm_quality_pipeline/defs/assets/country_layers.py
@@ -26,7 +26,6 @@ logger = dg.get_dagster_logger()
 @dg.asset(partitions_def=country_partitions, group_name="preparation")
 def country_layers(context, config: BoundaryConfig) -> dg.MaterializeResult[list[str]]:
     country = context.partition_key
-    logger.info(country)
 
     old_partitions = context.instance.get_dynamic_partitions("dynamic_country_layers")
     logger.info(f"existing partitions: {old_partitions}")
@@ -34,56 +33,61 @@ def country_layers(context, config: BoundaryConfig) -> dg.MaterializeResult[list
     out_dir = os.path.join("data", country)
     os.makedirs(out_dir, exist_ok=True)
 
-    updated_partitions= []
-
     if country == "DEU":
-        logger.info("download from BKG for Germany")
-
-        for level_val in config.bkg_boundary_levels:
-            download_url = f"{config.bkg_boundary_url}/DEU_{level_val}.gpkg"
-            logger.info(f"start download: {download_url}")
-            urllib.request.urlretrieve(
-                download_url,
-                f"{DATA_DIR}/DEU/DEU_{level_val}.gpkg"
-            )
-            updated_partitions.append(f"{country}|{level_val}")
-
-        create_h3_layer(country, f"{DATA_DIR}/DEU/DEU_vg25_sta.gpkg", out_dir)
-        updated_partitions.append(f"{country}|h3")
-        
-        context.instance.add_dynamic_partitions(
-            "dynamic_country_layers", updated_partitions
-        )
-
+        updated_partitions = country_layers_germany(context, config, country, out_dir)
     else:
-        logger.info("download from geoboundaries")
-
-        download_from_geoboundaries(
-            country=country,
-            level_val="boundaryType",
-            url_val="gjDownloadURL",
-            out_dir=out_dir,
-        )
-
-        adm0_boundary_path = os.path.join("data", country, f"{country}_adm0.gpkg")
-        create_h3_layer(country, adm0_boundary_path, out_dir)
-
-        updated_partitions = [
-            f"{country}|adm0",
-            f"{country}|adm1",
-            f"{country}|h3",
-        ]
-        context.instance.add_dynamic_partitions(
-            "dynamic_country_layers", updated_partitions
-        )
+        updated_partitions = country_layers_geoboundaries(context, country, out_dir)
 
     return dg.MaterializeResult(
         value=updated_partitions, metadata={"partitions": updated_partitions}
     )
 
 
-def download_from_geoboundaries(country, level_val, url_val, out_dir):
+def country_layers_geoboundaries(context, country, out_dir):
+    logger.info(f"download geo boundaries and create partitions for {country}")
+    download_from_geoboundaries(
+        country=country,
+        level_val="boundaryType",
+        url_val="gjDownloadURL",
+        out_dir=out_dir,
+    )
+    adm0_boundary_path = os.path.join("data", country, f"{country}_adm0.gpkg")
+    create_h3_layer(country, adm0_boundary_path, out_dir)
+    updated_partitions = [
+        f"{country}|adm0",
+        f"{country}|adm1",
+        f"{country}|h3",
+    ]
+    context.instance.add_dynamic_partitions(
+        "dynamic_country_layers", updated_partitions
+    )
+    return updated_partitions
 
+
+def country_layers_germany(context, config, country, out_dir):
+    logger.info("download BKG boundaries and create partitions for Germany")
+    updated_partitions = []
+
+    for level_val in config.bkg_boundary_levels:
+        download_url = f"{config.bkg_boundary_url}/DEU_{level_val}.gpkg"
+        logger.info(f"start download: {download_url}")
+        urllib.request.urlretrieve(
+            download_url,
+            f"{DATA_DIR}/DEU/DEU_{level_val}.gpkg"
+        )
+        updated_partitions.append(f"{country}|{level_val}")
+
+    create_h3_layer(country, f"{DATA_DIR}/DEU/DEU_vg25_sta.gpkg", out_dir)
+    updated_partitions.append(f"{country}|h3")
+
+    context.instance.add_dynamic_partitions(
+        "dynamic_country_layers", updated_partitions
+    )
+
+    return updated_partitions
+
+
+def download_from_geoboundaries(country, level_val, url_val, out_dir):
     list_url = f"https://www.geoboundaries.org/api/current/gbOpen/{country}/ALL"
     print(f"Fetching available levels from {list_url}")
     try:
