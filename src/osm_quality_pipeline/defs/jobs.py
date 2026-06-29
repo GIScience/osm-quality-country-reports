fc08fd74cbbca75f57b7f375ce390d3b52192291
vboehmer <valentin.boehmer@heigit.org>
2026-06-29 12:56:39 +0200

    fix: all_topics_job

--- src/osm_quality_pipeline/defs/jobs.py
+++ src/osm_quality_pipeline/defs/jobs.py
@@ -5,8 +5,8 @@ from osm_quality_pipeline.defs.partitions import dynamic_country_layers_partitio
 
 all_topics_job = dg.define_asset_job(
     name="all_topics_job",
-    description="Calculate all quality indicators for the topics building-count, roads and land-cover",
-    selection='group:"building_count" or group:"roads" or group:"land_cover, group:"schools" or group:"hospitals"',
+    description="Calculate all quality indicators for the topics building-count, roads, land-cover, schools and hospitals",
+    selection='group:"building_count" or group:"roads" or group:"land_cover" or group:"schools" or group:"hospitals"',
     partitions_def=dynamic_country_layers_partition
 )
 
