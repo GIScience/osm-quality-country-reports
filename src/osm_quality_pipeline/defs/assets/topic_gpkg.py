2f65cb9b85aa22fd284795b4ff90d2b34fd38d70
vboehmer <valentin.boehmer@heigit.org>
2026-07-09 11:51:41 +0200

    feat: topic_gpkg

---
+++ src/osm_quality_pipeline/defs/assets/topic_gpkg.py
@@ -0,0 +1,32 @@
+import dagster as dg
+import pandas as pd
+import geopandas as gpd
+
+from osm_quality_pipeline.defs.partitions import dynamic_country_layers_partition, get_country_layer_from_partitionkey
+from osm_quality_pipeline.defs.utils.oqapi import oqapi_requests
+from osm_quality_pipeline.defs.utils.utils import load_layer_as_gdf
+
+logger = dg.get_dagster_logger()
+
+@dg.asset(
+    partitions_def=dynamic_country_layers_partition,
+    name="topic_gpkg",
+    group_name="building_count",
+    deps=["building_count_attribute_completeness", "building_count_mapping_saturation", "building_count_user_activity", "building_count_currentness"],
+    metadata={
+        "partition_expr": "partition_key"  # DuckDB maps partitions to the 'partition_key' column
+    },
+    io_manager_key="duckdb_io_manager"
+)
+def topic_gpkg(context: dg.AssetExecutionContext, building_count_attribute_completeness, building_count_mapping_saturation, building_count_user_activity, building_count_currentness) -> None:
+    
+    df = pd.concat([
+        building_count_attribute_completeness,
+        building_count_mapping_saturation,
+        building_count_user_activity,
+        building_count_currentness
+    ])
+
+    df.geometry = gpd.GeoSeries.from_wkb(df.geometry.apply(bytes.fromhex), crs="EPSG:4326")
+    gdf = gpd.GeoDataFrame(df, geometry=df.geometry, crs="EPSG:4326")
+    gdf.to_file(f"data/{context.partition_key}/Outputs/building_count.gpkg", layer="building_count", driver="GPKG")
