38774505ca1d7d02c687936a500c065f9315ab95
JanReifenrath <jtr.linse@gmail.com>
2026-07-22 12:37:53 +0200

    feat: add building comparison indicator to building count topic group

---
+++ src/osm_quality_pipeline/defs/assets/building_comparison.py
@@ -0,0 +1,35 @@
+import dagster as dg
+import pandas as pd
+
+from osm_quality_pipeline.defs.partitions import dynamic_country_layers_partition, get_country_layer_from_partitionkey
+from osm_quality_pipeline.defs.utils.oqapi import oqapi_requests
+from osm_quality_pipeline.defs.utils.utils import load_layer_as_gdf
+
+logger = dg.get_dagster_logger()
+
+
+@dg.asset(
+    partitions_def=dynamic_country_layers_partition,
+    group_name="building_count",
+    deps=["country_layers"],
+    metadata={
+        "partition_expr": "partition_key"  # DuckDB maps partitions to the 'partition_key' column
+    },
+    io_manager_key="duckdb_io_manager"
+)
+def building_comparison(
+        context: dg.AssetExecutionContext,
+) -> pd.DataFrame:
+    INDICATOR = "building-comparison"
+    TOPIC = "building-area"
+
+    country_layer = get_country_layer_from_partitionkey(context.partition_key)
+    country = country_layer.country
+    layer = country_layer.layer
+
+    gdf = load_layer_as_gdf(context, country, layer)
+
+    df = oqapi_requests(gdf=gdf, topic=TOPIC, indicator=INDICATOR)
+    df["value"] = df["value"].round(4)
+    df["topic"] = "building-count" # give it the same topic name as others from the group to avoid confusion (should we do it like this???)
+    return df
