2d0e2a59d985ffd2eb9fb5252f5629347e5f94f3
JanReifenrath <jtr.linse@gmail.com>
2026-07-29 16:37:47 +0200

    update uv lock and lineage/dependencies for hdx asset

--- src/osm_quality_pipeline/defs/assets/build_HDX.py
+++ src/osm_quality_pipeline/defs/assets/build_HDX.py
@@ -6,7 +6,8 @@ from osm_quality_pipeline.defs.utils.hdx import get_s3_links
 from osm_quality_pipeline.defs.utils.hdx import upload_to_hdx
 logger = dg.get_dagster_logger()
 
-@dg.asset(partitions_def=country_partitions, group_name="uploads")
+
+@dg.asset(partitions_def=country_partitions, group_name="HDX_upload", deps=["upload_to_s3"])
 def hdx_upload(context, config: S3Config, s3: S3Resource):
     country = context.partition_key
     links_list = get_s3_links(config, country, s3)
