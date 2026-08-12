1de31e16f0079b43cf2fa34086d1ed6dc5b1212d
Benjamin Herfort <Hagellach37@users.noreply.github.com>
2026-08-12 11:33:40 +0200

    refactor jobs

--- src/osm_quality_pipeline/defs/jobs.py
+++ src/osm_quality_pipeline/defs/jobs.py
@@ -3,54 +3,25 @@ import dagster as dg
 from osm_quality_pipeline.defs.partitions import dynamic_country_layers_partition
 
 
-all_topics_job = dg.define_asset_job(
-    name="all_topics_job",
-    description="Calculate all quality indicators for the topics building-count, roads, land-cover, schools and hospitals",
+ohsome_quality_api_requests = dg.define_asset_job(
+    name="ohsome_quality_api_job",
+    description="Calculate all quality indicators for the topics building-count, roads, land-cover, schools and hospitals.",
     selection='group:"building_count" or group:"roads" or group:"land_cover" or group:"schools" or group:"hospitals"',
     partitions_def=dynamic_country_layers_partition
 )
 
-
-building_count_job = dg.define_asset_job(
-    name="building_count_job",
-    description="Calculate all quality indicators for the topic building-count",
-    selection='group:"building_count"',
-    partitions_def=dynamic_country_layers_partition
-)
-
-
-roads_job = dg.define_asset_job(
-    name="roads_job",
-    description="Calculate all quality indicators for the topic roads",
-    selection='group:"roads"',
-    partitions_def=dynamic_country_layers_partition
-)
-
-
-land_cover_job = dg.define_asset_job(
-    name="land_cover_job",
-    description="Calculate all quality indicators for the topic land-cover",
-    selection='group:"land_cover"',
-    partitions_def=dynamic_country_layers_partition
-)
-
-schools_job = dg.define_asset_job(
-    name="schools_job",
-    description="Calculate all quality indicators for the topic schools",
-    selection='group:"schools"',
-    partitions_def=dynamic_country_layers_partition
-)
-
-hospitals_job = dg.define_asset_job(
-    name="hospitals_job",
-    description="Calculate all quality indicators for the topic hospitals",
-    selection='group:"hospitals"',
-    partitions_def=dynamic_country_layers_partition
-)
-
-all_outputs_job = dg.define_asset_job(
-    name="all_outputs_job",
-    description="Generates csv and gpgk outputs",
+s3_upload = dg.define_asset_job(
+    name="s3_upload_job",
+    description="Generates csv and gpgk outputs and uploads to S3 bucket.",
     selection='group:"outputs"',
     partitions_def=dynamic_country_layers_partition
 )
+
+
+full_workflow = dg.define_asset_job(
+    name="full_workflow_job",
+    description="Calculate all quality indicators and upload result files to S3 bucket.",
+    selection='group:outputs or group:"building_count" or group:"roads" or group:"land_cover" or group:"schools" or group:"hospitals"',
+    partitions_def=dynamic_country_layers_partition
+)
+
