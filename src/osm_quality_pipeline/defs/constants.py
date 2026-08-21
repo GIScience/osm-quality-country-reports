ce543ade0b9bb689f526d746ba637d01d0d99139
Benjamin Herfort <Hagellach37@users.noreply.github.com>
2026-08-21 16:01:14 +0200

    add data layer for kreise in germany

--- src/osm_quality_pipeline/defs/constants.py
+++ src/osm_quality_pipeline/defs/constants.py
@@ -47,7 +47,7 @@ class BoundaryConfig(dg.Config):
     bkg_boundary_url: str = (
         f"https://storage.heigit.org/heigit-hdx-public/oqapi_hdx/boundaries"
     )
-    bkg_boundary_levels: [str] = ["vg2500_sta", "vg2500_lan", "vg25_gem"]
+    bkg_boundary_levels: [str] = ["vg2500_sta", "vg2500_lan", "vg1000_krs", "vg25_gem"]
     geoboundaries_levels: [str] = ["ADM0", "ADM1"]
 
 
