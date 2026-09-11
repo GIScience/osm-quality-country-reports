e1841dddcd203a925041300a0071867ec4d47c02
vboehmer <valentin.boehmer@heigit.org>
2026-09-11 15:12:03 +0200

    feat: updated jobs

--- src/osm_quality_pipeline/defs/jobs.py
+++ src/osm_quality_pipeline/defs/jobs.py
@@ -11,25 +11,10 @@ country_preparation = dg.define_asset_job(
 )
 
 
-ohsome_quality_api_requests = dg.define_asset_job(
-    name="ohsome_quality_api_job",
-    description="Calculate all quality indicators for the topics building-count, roads, land-cover, schools and hospitals.",
-    selection='group:"building_count" or group:"roads" or group:"land_cover" or group:"schools" or group:"hospitals"',
-    partitions_def=dynamic_country_layers_partition
-)
-
-s3_upload = dg.define_asset_job(
-    name="s3_upload_job",
-    description="Generates csv and gpgk outputs and uploads to S3 bucket.",
-    selection='group:"outputs"',
-    partitions_def=dynamic_country_layers_partition
-)
-
-
 full_workflow = dg.define_asset_job(
     name="full_workflow_job",
     description="Calculate all quality indicators and upload result files to S3 bucket.",
-    selection='group:outputs or group:"building_count" or group:"roads" or group:"land_cover" or group:"schools" or group:"hospitals"',
+    selection='group:outputs or group:"building_count" or group:"roads" or group:"land_cover" or group:"schools" or group:"hospitals" or group:"buildings"',
     partitions_def=dynamic_country_layers_partition
 )
 
